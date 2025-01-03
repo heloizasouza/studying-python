524a82ff6ef08e2d0672ac75a17dce28a31f1f89
Heloiza Souza <heloiza.oliveira.souza@gmail.com>
2025-01-03 09:45:17 -0300

    Verificação de Número Par ou Ímpar

---
+++ Sandeco-book/cap2ex11.py
@@ -0,0 +1,13 @@
+'''
+Verificação de Número Par ou Ímpar 
+Crie um programa que solicite ao usuário um número inteiro e verifique se ele é par ou ímpar, exibindo uma mensagem correspondente
+'''
+
+num = int(input('Digite um número: '))
+
+if num % 2 == 0:
+    print('O número é par')
+else:
+    print('O número é ímpar')
+
+
