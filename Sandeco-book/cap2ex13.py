bf7e12b2309fa15eb3b8106ab0e40740c7b1dd88
Heloiza Souza <heloiza.oliveira.souza@gmail.com>
2025-01-03 10:14:46 -0300

    Update cap2ex13.py

--- Sandeco-book/cap2ex13.py
+++ Sandeco-book/cap2ex13.py
@@ -1,4 +1,8 @@
-
+'''
+Conversão de Moeda
+Crie um programa que pergunte ao usuário a quantia em reais (BRL) e a taxa de câmbio atual para dólares (USD).
+Calcule e exiba o valor convertido em dólares.
+'''
 
 real = float(input('Digite a quantia em reais (BRL): '))
 cotacao = float(input('Digite a cotação do dólar (USD): '))
